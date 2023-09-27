999afa2df74fd47b134e7e3fbcefa0f14fd1fed4
Erwann Mest <m@kud.io>
2023-09-27 19:36:26 +0100

    docs: Add meta description and keywords to the document head

--- pages/_document.js
+++ pages/_document.js
@@ -12,6 +12,14 @@ class MyDocument extends Document {
             rel="stylesheet"
           />
           <title>SmartTV</title>
+          <meta
+            name="description"
+            content="SmartTV - Elevate your viewing experience. Explore a world of endless entertainment, immersive visuals, and smart innovations. Dive into the future of television with SmartTV."
+          />
+          <meta
+            name="keywords"
+            content="SmartTV, Next-gen TV, 4K, UHD, Streaming, Intelligent Television, Entertainment, Immersive Experience, TV Apps"
+          />
         </Head>
         <body>
           <Main />
