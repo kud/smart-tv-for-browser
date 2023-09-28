aacf1d7b68467cbbb555efefbd52f7e618b9fa91
Erwann Mest <m@kud.io>
2023-09-28 02:12:25 +0100

    feat(service-grid): Add keyboard navigation for service grid items

--- components/service-grid/index.js
+++ components/service-grid/index.js
@@ -1,23 +1,40 @@
 import styles from "styles/Home.module.css"
 
-const ServiceGrid = ({ servicesConfig, selectedServices }) => (
-  <div className={styles.container}>
-    {Object.keys(servicesConfig).map(
-      (serviceKey) =>
-        selectedServices[serviceKey] && (
-          <a key={serviceKey} href={servicesConfig[serviceKey].link}>
-            <div
-              className={styles.item}
-              style={{
-                backgroundColor: servicesConfig[serviceKey].backgroundColor,
-              }}
+const ServiceGrid = ({ servicesConfig, selectedServices }) => {
+  const handleKeyPress = (event, link) => {
+    if (event.key === "Enter" || event.key === " ") {
+      event.preventDefault()
+      window.location.href = link
+    }
+  }
+
+  return (
+    <div className={styles.container}>
+      {Object.keys(servicesConfig).map(
+        (serviceKey) =>
+          selectedServices[serviceKey] && (
+            <a
+              className={styles.itemLink}
+              key={serviceKey}
+              tabIndex="0"
+              onKeyDown={(event) =>
+                handleKeyPress(event, servicesConfig[serviceKey].link)
+              }
+              href={servicesConfig[serviceKey].link}
             >
-              <img alt="" src={servicesConfig[serviceKey].logo} />
-            </div>
-          </a>
-        ),
-    )}
-  </div>
-)
+              <div
+                className={styles.item}
+                style={{
+                  backgroundColor: servicesConfig[serviceKey].backgroundColor,
+                }}
+              >
+                <img alt="" src={servicesConfig[serviceKey].logo} />
+              </div>
+            </a>
+          ),
+      )}
+    </div>
+  )
+}
 
 export default ServiceGrid
